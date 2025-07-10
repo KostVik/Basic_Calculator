fc515cc7bfbc21410b4bb71b2931b0bf9e731267
Viktor <vkostenko276@gmail.com>
2025-07-10 14:50:12 +0300

    Rewrite Calc with classes Calculator

--- Basic_Calculator.py
+++ Basic_Calculator.py
@@ -1,16 +1,57 @@
-def add(first_num, second_num):
-    return first_num + second_num
+import math
 
-def substract(first_num, second_num):
-    return first_num - second_num
+class Calculator:
+    def __init__(self):
+        self.history = [] #the list of calculation history
 
-def multiply(first_num, second_num):
-    return first_num * second_num
+    def add(self, first_num, second_num):
+        result = first_num + second_num
+        self.history.append(f"{first_num} + {second_num} = {result}")
+        return result
 
-def divide(first_num, second_num):
-    if second_num == 0:
-        return "ERROR! You can not divide on Zero!"
-    return first_num / second_num
+    def substract(self, first_num, second_num):
+        result = first_num - second_num
+        self.history.append(f"{first_num} - {second_num} = {result}")
+        return result
+
+    def multiply(self, first_num, second_num):
+        result = first_num * second_num
+        self.history.append(f"{first_num} * {second_num} = {result}")
+        return result
+
+    def divide(self, first_num, second_num):
+        if second_num == 0:
+            return "ERROR! You can not divide by Zero!"
+        result = first_num / second_num
+        self.history.append(f"{first_num} / {second_num} = {result}")
+        return result
+    
+    def power(self, first_num, second_num):
+        result = first_num ** second_num
+        self.history.append(f"{first_num} ^ {second_num} = {result}")
+        return result
+    
+    def sqaure_root(self, first_num):
+        if first_num < 0:
+            return "ERROR! Square root of a negative number!"
+        result = math.sqrt(first_num)
+        self.history.append(f"√{first_num} = {result}")
+        return result
+    
+    def sin(self, first_num):
+        result = math.sin(math.radians(first_num)) #Converting degrees to radians
+        self.history.append(f"sin({first_num}°) = {result}")
+        return result
+    
+    def cos(self, first_num):
+        result = math.cos(math.radians(first_num)) #Converting degrees to radians
+        self.history.append(f"sin({first_num}°) = {result}")
+        return result
+    
+    def show_history(self):
+        if not self.history:
+            return "The History is empty."
+        return "\n".join(self.history)
 
 def basic_calculator():
     print("Please enter two numbers")
