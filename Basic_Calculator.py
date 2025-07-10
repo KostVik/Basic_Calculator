9500be9f6be64a95ef4edfe24ec8a2b5c0e6e40b
Viktor <vkostenko276@gmail.com>
2025-07-10 14:51:53 +0300

    Deleted old Basic Calculator part

--- Basic_Calculator.py
+++ Basic_Calculator.py
@@ -53,29 +53,5 @@ class Calculator:
             return "The History is empty."
         return "\n".join(self.history)
 
-def basic_calculator():
-    print("Please enter two numbers")
-    try:
-        first_num = float(input("First num:"))
-        second_num = float(input("Second num:"))
-    except ValueError:
-        return "ERROR! Please enter numeric values"
 
-    operation = input("Please choose operation(+, -, *, /): ")
-
-    if operation == '+':
-        result = add(first_num, second_num)
-    elif operation == '-':
-        result = substract(first_num, second_num)
-    elif operation == '*':
-        result = multiply(first_num, second_num)
-    elif operation == '/':
-        result = divide(first_num, second_num)
-    else:
-        result = "ERROR! Wrong operation!"
-
-    return f"Answer: {result}"
-
-#Start the Calculator
-print (basic_calculator())
 
